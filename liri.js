48f43c44a6f6aa30875da38828eb62edf2c2961f
Grace Gude <grace@Graces-MacBook-Pro.local>
2018-12-09 15:33:20 -0700

    Updating js

--- liri.js
+++ liri.js
@@ -2,9 +2,9 @@
 // require("dotenv").config();
 var command = process.argv[2];
 // var fs = require("fs");
-operator = process.argv.slice(3).join(" ");
+var operator = process.argv.slice(3).join(" ");
 
-function concertThis() {
+function concertThis(artist) {
     var moment = require('moment');
     var axios = require("axios");
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -13,9 +13,7 @@ function concertThis() {
             for (var i = 0; i < events.data.length; i++) {
                 var dateTime = events.data[i].datetime;
                 var eventDate = moment(dateTime);
-                console.log(`Venue Name: ${events.data[i].venue.name}`);
-                console.log(`Venue Location: ${events.data[i].venue.city}, ${events.data[i].venue.country}`);
-                console.log(`Date of the Event: ${eventDate.format("MM/DD/YYYY")}\n`);
+                console.log(`Artist: ${events.data[i].lineup} \nVenue Name: ${events.data[i].venue.name} \nVenue Location: ${events.data[i].venue.city}, ${events.data[i].venue.country} \nDate of the Event: ${eventDate.format("MM/DD/YYYY")}\n`);
             }
         }
     );
@@ -31,38 +29,51 @@ function spotifyThis(song) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log(data.tracks.items);
+        var data = data.tracks.items;
+        for (var i = 0; i < data.length; i++) {
+            console.log(`Artist: ${data[i].album.artists[0].name} \nSong Title: ${data[i].name} \nSpotify Preview Link: ${data[i].preview_url} \nAlbum Title: ${data[i].album.name}\n`);
+        }
     });
-
-    // Artist(s)
-    // The song's name
-    // A preview link of the song from Spotify
-    // The album that the song is from
-
-    // If no song is provided then your program will default to "The Sign" by Ace of Base.
 }
 
-function movieThis() {
+function movieThis(movie) {
     var axios = require("axios");
     axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             console.log(
                 `Movie title: ${response.data.Title} \nReleased: ${response.data.Year} \nIMDB Rating: ${response.data.imdbRating} \nRotten Tomatoes Rating: ${response.data.Ratings[1].Value} \nProduced in: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors: ${response.data.Actors}`
-
             );
         }
     );
 }
 
 function doThis() {
-    // if "do-what-it-says"
     var fs = require("fs");
     fs.readFile("random.txt", "utf8", function (err, data) {
-        // If the code experiences any errors it will log the error to the console.
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log(data);
+        var newSearch = data.split(",");
+        var newCommand = newSearch[0];
+        var newOperator = newSearch[1];
+
+        switch (newCommand) {
+            case "concert-this":
+                var artist = newOperator;
+                concertThis(artist);
+                break;
+            case "spotify-this-song":
+                var song = newOperator;
+                spotifyThis(song);
+                break;
+            case "movie-this":
+                var movie = newOperator;
+                movieThis(movie); 
+                break;
+            default:
+                console.log("Hit default!");
+                return false;
+        }
     });
 }
 
@@ -73,6 +84,10 @@ switch (command) {
         break;
     case "spotify-this-song":
         var song = operator
+        // If no song is provided then your program will default to "The Sign" by Ace of Base.
+        if (!operator) {
+            song = "The Sign Ace of Base";
+        }
         spotifyThis(song);
         break;
     case "movie-this":
